3ffb74af3b2ccc7304a6c96ede77bfed9b5782bb
Matt Hodgman <matthodg1@gmail.com>
2021-07-15 14:41:29 -0600

    hard coded file name

--- control.sh
+++ control.sh
@@ -1,11 +1,10 @@
 num_jobs=$1 # the number of jobs to submit
-chembl=$2 # chembl_indications.tsv
 
 # make drug list
-cut -d$'\t' -f2 ${chembl} | sort | uniq > drugs
+cut -d$'\t' -f2 data/chembl_indications.tsv | sort | uniq > drugs
 
 # split drug list into a file for each job
-split -n l/${num_jobs} drugs 'drugs_'
+split -n l/${num_jobs} drugs 'data/drugs_'
 
 # run a job on each drug list
-ls drugs_* | xargs -I {} sbatch job.sh -d {} -a drugs -g chembl.gpickle -p drug_node_dict.pickle
+ls data/drugs_* | xargs -I {} sbatch child.sh {}
